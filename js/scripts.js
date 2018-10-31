2e1bb0794587b3c3ab8dab9cbd225d0ecb9d7408
Cole Marsteller, Miles Bradt and ngan nguyen <cole@>
2018-10-31 14:18:56 -0700

    add second player to UI output

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-function Player(turnTotal, finalTotal) {
+function Player(turnTotal, finalTotal, id) {
   this.turnTotal = turnTotal,
-  this.finalTotal = finalTotal
+  this.finalTotal = finalTotal,
+  this.id = id
 }
 
-function PlayerLog() {
-  this.players = [];
-  this.currentId = 0;
-}
+// function PlayerLog() {
+//   this.players = [];
+//   this.currentId = 0;
+// }
 
-PlayerLog.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-PlayerLog.prototype.arraysPlayers = function(player) {
-  player.id = this.assignId();
-  this.players.push(player);
-}
+// PlayerLog.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+//
+// PlayerLog.prototype.arraysPlayers = function(player) {
+//   player.id = this.assignId();
+//   this.players.push(player);
+// }
 
 Player.prototype.diceRNGRoll = function(max = 7, min = 1) {
   var diceRoll = Math.floor(Math.random() * (max - min)) + min;
@@ -49,21 +50,40 @@ Player.prototype.finalTotalHolder = function() {
   }
 }
 
-var playerLog = new PlayerLog();
-var newPlayer1 = new Player(0, 0);
-var newPlayer2 = new Player(0, 0);
+// var playerLog = new PlayerLog();
+var newPlayer1 = new Player(0, 0, 1);
+var newPlayer2 = new Player(0, 0, 0);
 
 $(document).ready(function() {
   $(".roll").click(function() {
-    $("#dice-roll").text(newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll()));
-    console.log(newPlayer1);
-    $("#turn-total-view").text("Turn total: " + newPlayer1.turnTotal);
-    // document.getElementById('output').innerHTML = newPlayer1.diceRNGRoll();
+    if (newPlayer1.id === 1) {
+      $("#dice-roll-1").text(newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll()));
+      console.log(newPlayer1);
+      $("#turn-total-view1").text("Turn total: " + newPlayer1.turnTotal);
+      // document.getElementById('output').innerHTML = newPlayer1.diceRNGRoll();
+    } else if (newPlayer2.id === 1) {
+      $("#dice-roll-2").text(newPlayer2.turnTotalHolder(newPlayer2.diceRNGRoll()));
+      console.log(newPlayer1);
+      $("#turn-total-view2").text("Turn total: " + newPlayer2.turnTotal);
+    }
   })
 
+
   $(".hold").click(function() {
-    newPlayer1.finalTotalHolder(this.turnTotal);
-    
-    $("#final-total-view").text("Final Total: " + newPlayer1.finalTotal);
+    if (newPlayer1.id === 1) {
+      newPlayer1.finalTotalHolder(this.turnTotal);
+      $("#final-total-view1").text("Final Total: " + newPlayer1.finalTotal);
+      newPlayer1.id = newPlayer1.id - 1;
+      console.log(newPlayer1.id);
+      newPlayer2.id = newPlayer2.id + 1;
+      return console.log("player 2: " + newPlayer2.id + "player 1: " + newPlayer1.id);
+    } else if (newPlayer2.id === 1) {
+      newPlayer2.finalTotalHolder(this.turnTotal);
+      $("#final-total-view2").text("Final Total: " + newPlayer2.finalTotal);
+      newPlayer2.id = newPlayer2.id - 1;
+      console.log(newPlayer2.id);
+      newPlayer1.id = newPlayer1.id + 1;
+      return console.log("player 1: " + newPlayer1.id + "player 2: " + newPlayer2.id);
+    }
   })
 });
