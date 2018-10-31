ad7b42f3e2fcd4497637f6b5795522908327972d
Cole Marsteller, Miles Bradt and ngan nguyen <ngan@>
2018-10-31 10:46:46 -0700

    add dice roll prototype and ability for user to add to turn total

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-function UserData(turnTotal, finalTotal) {
+function Player(turnTotal, finalTotal) {
   this.turnTotal = turnTotal,
   this.finalTotal = finalTotal
 }
 
-UserData.prototype.diceRNG = function(max = 7, min = 1) {
+Player.prototype.diceRNGRoll = function(max = 7, min = 1) {
   var diceRoll = Math.floor(Math.random() * (max - min)) + min;
   console.log(diceRoll); // Turn into something the user can see
+  return diceRoll;
+}
+
+Player.prototype.turnTotalHolder = function (diceRoll) {
   return this.turnTotal = diceRoll + this.turnTotal;
 }
 
-
-var newUser = new UserData(0, 0);
+var newPlayer = new Player(0, 0);
 
 $(document).ready(function() {
 
