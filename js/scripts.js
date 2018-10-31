e1a00c16d881dc486d6e93f9894ea958397857ef
Cole Marsteller, Miles Bradt and ngan nguyen <cole@>
2018-10-31 15:23:48 -0700

    add next turn animation

--- js/scripts.js
+++ js/scripts.js
@@ -40,24 +40,34 @@ var newPlayer2 = new Player(0, 0, 0);
 $(document).ready(function() {
   $(".roll").click(function() {
     if (newPlayer1.id === 1) {
-      $("#dice-roll1").text(newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll()));
+      var diceRoll = newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll())
+      $("#dice-roll1").text(diceRoll);
+      if (diceRoll === 1) {
+        $("#player1Rolled1").fadeIn(800, "linear");
+        $("#player1Rolled1").fadeOut(800, "linear");
+      }
       console.log(newPlayer1);
       if (newPlayer1.id === 0) {
-        $("#turn-total-view1").text("Turn total: " + newPlayer1.turnTotal);
+        $("#turn-total-view1").text(newPlayer1.turnTotal);
         return newPlayer2.id = newPlayer2.id + 1;
       } else {
-        $("#turn-total-view1").text("Turn total: " + newPlayer1.turnTotal);
+        $("#turn-total-view1").text(newPlayer1.turnTotal);
         // document.getElementById('output').innerHTML = newPlayer1.diceRNGRoll();
       }
     }
     if (newPlayer2.id === 1) {
-      $("#dice-roll2").text(newPlayer2.turnTotalHolder(newPlayer2.diceRNGRoll()));
+      var diceRoll = newPlayer2.turnTotalHolder(newPlayer2.diceRNGRoll())
+      $("#dice-roll2").text(diceRoll);
+      if (diceRoll === 1) {
+        $("#player2Rolled1").fadeIn(800, "linear");
+        $("#player2Rolled1").fadeOut(800, "linear");
+      }
       console.log(newPlayer1);
         if (newPlayer2.id === 0) {
-          $("#turn-total-view2").text("Turn total: " + newPlayer2.turnTotal);
+          $("#turn-total-view2").text(newPlayer2.turnTotal);
           return newPlayer1.id = newPlayer1.id = 1;
         } else {
-      $("#turn-total-view2").text("Turn total: " + newPlayer2.turnTotal);
+      $("#turn-total-view2").text(newPlayer2.turnTotal);
       }
     }
   })
@@ -65,16 +75,20 @@ $(document).ready(function() {
   $(".hold").click(function() {
     if (newPlayer1.id === 1) {
       newPlayer1.finalTotalHolder(this.turnTotal);
-      $("#final-total-view1").text("Final Total: " + newPlayer1.finalTotal);
+      $("#final-total-view1").text(newPlayer1.finalTotal);
       newPlayer1.id = newPlayer1.id - 1;
       newPlayer2.id = newPlayer2.id + 1;
-      $("#turn-total-view1").text("Turn total: " + newPlayer2.turnTotal);
+      $("#turn-total-view1").text(newPlayer2.turnTotal);
+      $("#player1Rolled1").fadeIn(800, "linear");
+      $("#player1Rolled1").fadeOut(800, "linear");
     } else if (newPlayer2.id === 1) {
       newPlayer2.finalTotalHolder(this.turnTotal);
-      $("#final-total-view2").text("Final Total: " + newPlayer2.finalTotal);
+      $("#final-total-view2").text(newPlayer2.finalTotal);
       newPlayer2.id = newPlayer2.id - 1;
       newPlayer1.id = newPlayer1.id = 1;
-      $("#turn-total-view2").text("Turn total: " + newPlayer1.turnTotal);
+      $("#player2Rolled1").fadeIn(800, "linear");
+      $("#player2Rolled1").fadeOut(800, "linear");
+      $("#turn-total-view2").text(newPlayer1.turnTotal);
     }
   })
 });
