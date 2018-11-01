73e0f552cc5128277edbe891a137f8814e3e653f
cole marsteller and Miles Bradt <miles@>
2018-11-01 14:30:50 -0700

    add easy CPU to play against

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ Player.prototype.finalTotalHolder = function() {
 
 var newPlayer1 = new Player(0, 0, 1);
 var newPlayer2 = new Player(0, 0, 0);
-var newCPU = new Player(0, 0, 0);
+var newPlayerCPU = new Player(0, 0);
 
 $(document).ready(function() {
   $(".roll").click(function() {
@@ -68,33 +68,63 @@ $(document).ready(function() {
         $("#player1Rolled1").fadeOut(400, "linear");
       }
       console.log(newPlayer1);
-        if (newPlayer2.id === 0) {
-          $("#turn-total-view2").text(newPlayer2.turnTotal);
-          return newPlayer1.id = newPlayer1.id = 1;
-        } else {
-      $("#turn-total-view2").text(newPlayer2.turnTotal);
+      if (newPlayer2.id === 0) {
+        $("#turn-total-view2").text(newPlayer2.turnTotal);
+        return newPlayer1.id = newPlayer1.id = 1;
+      } else {
+        $("#turn-total-view2").text(newPlayer2.turnTotal);
       }
     }
-  })
+  });
 
-  // $(".rollCPU").click(function() {
-  //   var diceRoll = newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll())
-  //   $("#dice-roll1").text(diceRoll);
-  //   if (diceRoll === 1) {
-  //     $(".player2").addClass("player2Glow");
-  //     $(".player1").removeClass("player1Glow");
-  //     $("#player2Rolled1").fadeIn(800, "linear");
-  //     $("#player2Rolled1").fadeOut(400, "linear");
-  //   }
-  //   console.log(newPlayer1);
-  //   if (newPlayer1.id === 0) {
-  //     $("#turn-total-view1").text(newPlayer1.turnTotal);
-  //     return newPlayer2.id = newPlayer2.id + 1;
-  //   } else {
-  //     $("#turn-total-view1").text(newPlayer1.turnTotal);
-  //     // document.getElementById('output').innerHTML = newPlayer1.diceRNGRoll();
-  //   }
-  // })
+  $(".CPU").click(function(){
+    $(".rollCPU").toggle();
+    $(".cpuOutput").toggle();
+    $(".playerCPU").toggle();
+    $(".rollCPU").show();
+    $(".hold").toggle();
+    $(".holdCPU").toggle();
+    $(".player2").toggle();
+    $(".player2Output").toggle();
+    $(".roll").hide();
+  });
+
+  $(".rollCPU").click(function() {
+    var diceRoll = newPlayer1.turnTotalHolder(newPlayer1.diceRNGRoll())
+    $("#dice-roll1").text(diceRoll);
+    $("#turn-total-view1").text(newPlayer1.turnTotal);
+    if (diceRoll === 1) {
+      $(".player2").addClass("player2Glow");
+      $(".player1").removeClass("player1Glow");
+    }
+    console.log(newPlayer1);
+    if (newPlayer1.id === 0) {
+      $("#turn-total-view1").text(newPlayer1.turnTotal);
+      var diceRoll = newPlayerCPU.turnTotalHolder(newPlayerCPU.diceRNGRoll())
+      console.log("CPU Roll: " + diceRoll);
+      $("#cpuRoll1").text(diceRoll)
+      console.log(newPlayerCPU.turnTotal);
+      $("#dice-rollCPU").text(diceRoll);
+      if (diceRoll === 1) {
+        $(".playerCPU").addClass("playerCPUGlow");
+        $(".player1").removeClass("player1Glow");
+        return newPlayer1.id = newPlayer1.id + 1;
+      } else {
+        var diceRoll = newPlayerCPU.turnTotalHolder(newPlayerCPU.diceRNGRoll())
+        console.log("CPU Roll 2: " + diceRoll);
+        $("#cpuRoll2").text(diceRoll)
+        $("#turn-total-viewCPU").text(newPlayerCPU.turnTotal);
+        console.log(newPlayerCPU.turnTotal);
+        newPlayerCPU.finalTotalHolder(newPlayerCPU.turnTotal);
+        console.log(newPlayerCPU.finalTotal);
+        $("#final-total-viewCPU").text(newPlayerCPU.finalTotal);
+        $("#dice-rollCPU").text(diceRoll);
+        $(".playerCPU").addClass("playerCPUGlow");
+        $(".player1").removeClass("player1Glow");
+        return newPlayer1.id = newPlayer1.id + 1;
+      }
+    }
+  });
 
   $(".hold").click(function() {
     if (newPlayer1.id === 1) {
@@ -144,5 +174,36 @@ $(document).ready(function() {
         newPlayer2.id = 0
       }
     }
-  })
+  });
+
+  $(".holdCPU").click(function() {
+    if (newPlayer1.id === 1) {
+      newPlayer1.finalTotalHolder(this.turnTotal);
+      $("#final-total-view1").text(newPlayer1.finalTotal);
+      newPlayer1.id = 0;
+      if (newPlayer1.id === 0) {
+        $("#turn-total-view1").text(newPlayer1.turnTotal);
+        var diceRoll = newPlayerCPU.turnTotalHolder(newPlayerCPU.diceRNGRoll())
+        $("#cpuRoll1").text(diceRoll)
+        $("#dice-rollCPU").text(diceRoll);
+        if (diceRoll === 1) {
+          $(".playerCPU").addClass("playerCPUGlow");
+          $(".player1").removeClass("player1Glow");
+          return newPlayer1.id = newPlayer1.id + 1;
+        } else {
+          var diceRoll = newPlayerCPU.turnTotalHolder(newPlayerCPU.diceRNGRoll())
+          $("#cpuRoll2").text(diceRoll)
+          $("#turn-total-viewCPU").text(newPlayerCPU.turnTotal);
+          console.log(newPlayerCPU.turnTotal);
+          newPlayerCPU.finalTotalHolder(newPlayerCPU.turnTotal);
+          console.log(newPlayerCPU.finalTotal);
+          $("#final-total-viewCPU").text(newPlayerCPU.finalTotal);
+          $("#dice-rollCPU").text(diceRoll);
+          $(".playerCPU").addClass("playerCPUGlow");
+          $(".player1").removeClass("player1Glow");
+          return newPlayer1.id = newPlayer1.id + 1;
+        }
+      }
+    }
+  });
 });
